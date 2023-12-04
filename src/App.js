03da4c09c3f5f4a2d1ea25bcc319a024d5a94415
Luis Santos <luisguerreiro095@gmail.com>
2023-12-04 17:38:25 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Menu E-Commerce </code>.
+          <code>Menu E-Commerce.</code>
         </p>
         <a
           className="App-link"
