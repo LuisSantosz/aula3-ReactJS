efdc0df82ccb56c15f81f27ba20b27f85ea6bb9e
Luis Santos <luisguerreiro095@gmail.com>
2023-12-04 16:45:43 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Aula 3 - JSX e Transpiling </code>.
         </p>
         <a
           className="App-link"
